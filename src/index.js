bd5eab5b149f9f9daede8cdaad848a06f64deb0a
田宇奇 <tianyuqi@tianyuqideMacBook-Pro.local>
2020-09-15 22:48:49 +0800

    学习使用babel/preset-react来对react文件进行打包编译，babel这个loader配置项的执行顺序是从下到上，从右至左

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import "@babel/polyfill"
-import random from './console';
+// import "@babel/polyfill"
+// import random from './console';
 
-const obj = {
-    name: 'tianyuqi'
+// const obj = {
+//     name: 'tianyuqi'
+// }
+
+// if (module.hot) {
+//     module.hot.accept('./console.js', function () {
+//         random()
+//     })
+// }
+
+import React, { Component } from "react";
+import ReactDom from 'react-dom'
+
+class App extends Component {
+  render() {
+    return <div>hello world!</div>;
+  }
 }
 
-if (module.hot) {
-    module.hot.accept('./console.js', function () {
-        random()
-    })
-}
+ReactDom.render(<App/>,document.getElementById("root"));
